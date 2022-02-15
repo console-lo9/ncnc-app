952caa4c7287693fa232a91ce4730c94d713a8d4
ttaerrim <ltr0121@naver.com>
2022-02-16 04:54:09 +0900

    Add: e2e test 작성

--- cypress/integration/App.js
+++ cypress/integration/App.js
@@ -3,3 +3,34 @@ describe('Main Test', () => {
         cy.visit('/');
     });
 });
+
+describe('마이페이지 테스트', () => {
+    it('Click into button', () => {
+        cy.get(
+            '[style="box-sizing:border-box;display:inline-block;overflow:hidden;width:initial;height:initial;background:none;opacity:1;border:0;margin:0;padding:0;position:relative;max-width:100%"] > [alt="menu"]',
+        ).click();
+        cy.get(
+            '[src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FcloseButton.f2264892.svg&w=64&q=75"]',
+        ).click();
+    });
+});
+
+describe('메인 버튼 테스트', () => {
+    it('Click into button', () => {
+        cy.get(
+            ':nth-child(1) > .css-1v0qy5v > .css-5ra6k4 > .css-41tqbh',
+        ).click();
+        cy.get(':nth-child(1) > .css-n8sqfl').click();
+        cy.get('.css-1em771j').click();
+        cy.get(':nth-child(1) > .css-18nc8lv > .css-wkxndf').click();
+        cy.get('.css-dfjll8').click();
+    });
+});
+
+describe('뒤로 가기 테스트', () => {
+    it('Click into button', () => {
+        cy.get('.css-18tfhk1').click;
+        cy.get('.css-18tfhk1').click;
+        cy.get('.css-18tfhk1').click;
+    });
+});
